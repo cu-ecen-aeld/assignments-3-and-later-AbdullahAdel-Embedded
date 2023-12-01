eccbe4bb1b970a0f5054e27059ef8bf619a2f7d0
AbdullahAdel-Embedded <150817022+AbdullahAdel-Embedded@users.noreply.github.com>
2023-12-01 04:55:30 +0200

    Update autotest-validate.c

--- examples/autotest-validate/autotest-validate.c
+++ examples/autotest-validate/autotest-validate.c
@@ -27,6 +27,6 @@ bool this_function_returns_false()
  */
 const char *my_username()
 {
-    const char name[] = "AbdullahAdel-Embedded";
-    return name;
+     
+    return "AbdullahAdel-Embedded";
 }
