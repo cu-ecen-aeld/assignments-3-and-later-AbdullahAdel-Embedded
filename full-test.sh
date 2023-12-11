69baeb9c99f7a3568a3b53e3199c62c28f90d46a
AbdullahAdel-Embedded <abdullah.adel.g.h@gamil.com>
2023-12-11 11:05:40 +0200

    assignment-3 part-2

--- full-test.sh
+++ full-test.sh
@@ -4,15 +4,10 @@
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
-while true
-do
-    echo "Performing some task..."
-    
-    # Add your commands or tasks here
 
     # Sleep for 1 minute (60 seconds)
     sleep 60
-done
+
 
 mkdir -p build
 cd build
