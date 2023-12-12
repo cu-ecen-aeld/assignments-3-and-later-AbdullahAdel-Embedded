6c12fa58a28683c0468d3f590ba5bbc7490b43b2
AbdullahAdel-Embedded <abdullah.adel.g.h@gamil.com>
2023-12-12 09:42:22 +0200

    assignment-4 part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,7 +14,30 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    
+   struct thread_data* thread_func_args = (struct thread_data*)thread_param;
+
+    // Wait to obtain the mutex
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);  // Convert to microseconds
+
+    pthread_mutex_lock(thread_func_args->mutex);
+
+    // Perform operations that require the mutex
+
+    // Simulate some work
+    usleep(thread_func_args->wait_to_release_ms * 1000);  // Convert to microseconds
+
+    // Release the mutex
+    pthread_mutex_unlock(thread_func_args->mutex);
+
+    // Set the completion status to true
+    thread_func_args->thread_complete_success = true;
+
+    return thread_param;  // Returning the thread data structure
+
+    // Note: It's important that the thread data structure is dynamically allocated so that it survives
+    // beyond the scope of this function. The main function or another part of the program should
+    // eventually free this memory.
 }
 
 
@@ -28,6 +51,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    //return false;
+        struct thread_data* thread_args = malloc(sizeof(struct thread_data));
+
+    if (thread_args == NULL) {
+        perror("Error allocating memory for thread arguments");
+        return false;
+    }
+
+    thread_args->mutex = mutex;
+    thread_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_args->wait_to_release_ms = wait_to_release_ms;
+    thread_args->thread_complete_success = false;
+
+    // Create the thread and pass thread_args as a parameter
+    if (pthread_create(thread, NULL, threadfunc, (void*)thread_args) != 0) {
+        perror("Error creating thread");
+        free(thread_args);
+        return false;
+    }
+
+    return true;
 }
 
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -13,106 +13,134 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
-if [ $# -lt 1 ]; then
-    echo "Using default directory ${OUTDIR} for output"
+if [ $# -lt 1 ]
+then
+	echo "Using default directory ${OUTDIR} for output"
 else
-    OUTDIR=$1
-    echo "Using passed directory ${OUTDIR} for output"
+	OUTDIR=$1
+	echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    # Clone only if the repository does not exist.
-    echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-    git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # Add your kernel build steps here
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j$(nproc)
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_MOD_PATH=output modules_install
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} INSTALL_PATH=output install
+    # TODO: Add your kernel build steps here
+
+    #clean
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+
+    #configure 
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+
+    #make kernal 
+    make -j8 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+
+    #make modules 
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+
+    #build device tree 
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+
 fi
 
+#was this suppose to be a TODO?
 echo "Adding the Image in outdir"
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]; then
-    echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm -rf ${OUTDIR}/rootfs
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# Create necessary base directories
-echo "Creating necessary base directories in ${OUTDIR}/rootfs"
-mkdir -p ${OUTDIR}/rootfs
-cd ${OUTDIR}/rootfs
+# TODO: Create necessary base directories
+mkdir -p rootfs
+cd rootfs
 
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 
+
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]; then
-    git clone git://busybox.net/busybox.git
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    
-    # TODO: Configure busybox (you can add 'make menuconfig' here if needed)
-    
-    # Clean build artifacts for busybox
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    
-    # Build and install busybox
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
+    # TODO:  Configure busybox
+    #make distclean
+    #make defconfig
+    #configuring here means the config process won't redone as long as the git clone worked, so I done it in the next section. 
 else
     cd busybox
 fi
 
-# TODO: Add library dependencies to rootfs
-# Done
+# TODO: Make and install busybox
+make distclean
+make defconfig
+#Build
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+#create symlinks 
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+#go back to root
+cd ${OUTDIR}/rootfs
+
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+# TODO: Add library dependencies to rootfs
+LIBC=$(aarch64-none-linux-gnu-gcc -print-sysroot -v)
+#echo The value of libc is $LIBC
+cp $LIBC/lib64/libm.so.6 $OUTDIR/rootfs/lib64/libm.so.6
+cp $LIBC/lib64/libresolv.so.2 $OUTDIR/rootfs/lib64/libresolv.so.2
+cp $LIBC/lib64/libc.so.6 $OUTDIR/rootfs/lib64/libc.so.6
+
+cp $LIBC/lib/ld-linux-aarch64.so.1 $OUTDIR/rootfs/lib/ld-linux-aarch64.so.1
+
 
 # TODO: Make device nodes
-# Done
+cd $OUTDIR/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+
 
 # TODO: Clean and build the writer utility
-# Done
-
-cd /home/abdullah/Desktop/assignment-3/finder-app
-make clean
-make
+cd $FINDER_APP_DIR
+make clean 
+make CROSS_COMPILE=$CROSS_COMPILE writer
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-# Done
+cp build/writer $OUTDIR/rootfs/home/writer
+cp finder-test.sh $OUTDIR/rootfs/home/finder-test.sh
+cp finder.sh $OUTDIR/rootfs/home/finder.sh
+mkdir -p $OUTDIR/rootfs/home/conf
+cp conf/assignment.txt $OUTDIR/rootfs/home/conf/assignment.txt
+cp conf/username.txt $OUTDIR/rootfs/home/conf/username.txt
+cp autorun-qemu.sh $OUTDIR/rootfs/home/autorun-qemu.sh
 
-cp /home/abdullah/Desktop/assignment-3/finder-app/* ${OUTDIR}/rootfs/home/
 
-# TODO: Chown      S the root directory
-# Done
-cd ${OUTDIR}/rootfs
+# TODO: Chown the root directory
+cd $OUTDIR/rootfs
 sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-gzip -f ${OUTDIR}/initramfs.cpio
-
+cd $OUTDIR
+gzip -f initramfs.cpio
